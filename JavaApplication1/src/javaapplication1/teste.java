3f87b29ed8669c77aa8b9b525db380af8f586e4a
Ri <Ri@Ricardo>
2016-09-27 09:01:18 -0300

    vai da boa dessa vez consegui

---
+++ JavaApplication1/src/javaapplication1/teste.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Ri
+ */
+public class teste {
+    String e = "agora vai dar boa";
+
+    public String getE() {
+        return e;
+    }
+
+    public void setE(String e) {
+        this.e = e;
+    }
+    public void oi(){
+            System.out.println("vai dar boa ");
+    }
+
+}
